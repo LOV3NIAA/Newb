71acbea30013c01c792e777106cb0e30fb06be73
LOV3NIAA <155994803+LOV3NIAA@users.noreply.github.com>
2024-09-09 08:21:29 -0500

    Nia's code

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Title of Document</title>
+</head>
+
+<body>
+    <form method= "GET">
+        <input type= "text" name="person">
+        <button>SUBMIT</button>
+    </form>
+
+   <?php
+    $name ="Nia";
+    echo $name." is a silly willy goose!";
+    ?>
+</body>
+
+</html>
