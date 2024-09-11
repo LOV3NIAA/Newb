2bd5ff55cd55c46dbc85e36848f9492bf3e7cc81
LOV3NIAA <155994803+LOV3NIAA@users.noreply.github.com>
2024-09-11 07:57:04 -0500

    code

---
+++ datatypes.php
@@ -0,0 +1,30 @@
+!DOCTYPE html>
+<html>
+<head>
+    <title>Title of Document</title>
+</head>
+
+<body>
+<?php
+
+    //String
+    $name = 'Robotics Rock';
+
+    //Integer
+    $name = 10;
+    
+    //
+    $name =10.5874
+
+    //Boolean
+    true = 1
+    false = 0
+
+    //Array
+    $names = array ("Ania", "Kristine", "Ross")
+    echo $names['2'];
+
+    ?>
+</body>
+
+</html>
